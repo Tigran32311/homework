94514626a012990e77f191beedc752cbf3efd61c
Tigran32311 <tigran32311@mail.ru>
2022-10-24 13:44:16 +0300

    Выполнение пункта 2, минимальное веб-приложение, подсчитывающее сумму двух элементов, вводимых пользователем

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
+<form action="index.php">
+    <label>Первое число</label>
+    <input type="text" name="first_num" >
+    <p>+</p>
+    <label>Второе число</label>
+    <input type="text" name="second_num">
+    <button type="submit"> Resolve </button>
+</form>
 
-?>
+<?php
+if (isset($_GET)) {
+    echo $_GET['first_num']+$_GET['second_num'];
+}
