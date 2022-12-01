b830835e8ecf886081204f8b0fba96fbfcfe4bb3
Tigran32311 <tigran32311@mail.ru>
2022-12-01 16:01:43 +0300

    Домашнее задание №2, работа с моделями. Создание моделей и миграций, добавление вторичных ключей для связывания таблиц

---
+++ database/migrations/2022_12_01_125903_add_foreign_keys_to_doctors.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            //Вторичные ключи, для id из таблиц "specializations" и "posts"
+            $table->foreignId('Specialization_id')->references('id')->on('specializations');
+            $table->foreignId('Post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            //
+        });
+    }
+};
